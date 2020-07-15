7b2b3c62ccc3fb296afbcf2f32f6ceb5098e937f
ErickESC <eekoston@gmail.com>
2020-07-14 23:05:38 -0500

    prueba war

--- src/main/java/mx/uam/tsinsoft/adoptPokemon/AdoptPokemonApplication.java
+++ src/main/java/mx/uam/tsinsoft/adoptPokemon/AdoptPokemonApplication.java
@@ -2,10 +2,20 @@ package mx.uam.tsinsoft.adoptPokemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
 
 @SpringBootApplication
-public class AdoptPokemonApplication {
+public class AdoptPokemonApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(AdoptPokemonApplication.class);
+    }
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AdoptPokemonApplication.class, args);
 	}
